1a1a631a8ce3d716bfc96f3a6e8324ef48c384c3
saddem <saddem@172.16.66.169>
2016-06-12 15:25:01 +0100

    1*1

--- ejbModule/com/esprit/entity/Boutique.java
+++ ejbModule/com/esprit/entity/Boutique.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToOne;
 @Entity
 public class Boutique implements Serializable{
 	
-	private int id;
+	private int id22;
 	private String localisation;
 	private int tel;
 	private ProprietaireBoutique pb;
